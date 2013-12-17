57c06ef8f99d71adfee018568768efae05909540
Alexander Logger <intagger@live.ru>
2013-12-17 14:12:56 +0400

    Added ToByteArray(string) method to StringUtils. Added PepitaGet packages restoring for the test project.

--- src/BigMath/BigMath.PCL/Utils/StringUtils.cs
+++ src/BigMath/BigMath.PCL/Utils/StringUtils.cs
@@ -4,11 +4,12 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Text;
 
 namespace BigMath.Utils
 {
-    internal static class StringUtils
+    public static class StringUtils
     {
         public static string ToHexaString(ulong[] values, bool caps, int min)
         {
@@ -26,6 +27,85 @@ namespace BigMath.Utils
             if (dif > 0)
             {
                 value = new string('0', min - value.Length) + value;
+            }
+
+            return value;
+        }
+
+        public static byte[] ToByteArray(this string value)
+        {
+            byte[] bytes = null;
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                bytes = new byte[0];
+            }
+            else
+            {
+                int stringLength = value.Length;
+                int characterIndex = (value.StartsWith("0x", StringComparison.Ordinal)) ? 2 : 0;
+                    // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
+                int numberOfCharacters = stringLength - characterIndex;
+
+                bool addLeadingZero = false;
+                if (0 != (numberOfCharacters%2))
+                {
+                    addLeadingZero = true;
+
+                    numberOfCharacters += 1; // Leading '0' has been striped from the string presentation.
+                }
+
+                bytes = new byte[numberOfCharacters/2]; // Initialize our byte array to hold the converted string.
+
+                int writeIndex = 0;
+                if (addLeadingZero)
+                {
+                    bytes[writeIndex++] = FromCharacterToByte(value[characterIndex], characterIndex);
+                    characterIndex += 1;
+                }
+
+                for (int readIndex = characterIndex; readIndex < value.Length; readIndex += 2)
+                {
+                    byte upper = FromCharacterToByte(value[readIndex], readIndex, 4);
+                    byte lower = FromCharacterToByte(value[readIndex + 1], readIndex + 1);
+
+                    bytes[writeIndex++] = (byte) (upper | lower);
+                }
+            }
+
+            return bytes;
+        }
+
+        public static string Reverse(this string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new String(chars);
+        }
+
+        private static byte FromCharacterToByte(char character, int index, int shift = 0)
+        {
+            var value = (byte) character;
+            if (((0x40 < value) && (0x47 > value)) || ((0x60 < value) && (0x67 > value)))
+            {
+                if (0x40 == (0x40 & value))
+                {
+                    if (0x20 == (0x20 & value))
+                    {
+                        value = (byte) (((value + 0xA) - 0x61) << shift);
+                    }
+                    else
+                    {
+                        value = (byte) (((value + 0xA) - 0x41) << shift);
+                    }
+                }
+            }
+            else if ((0x29 < value) && (0x40 > value))
+            {
+                value = (byte) ((value - 0x30) << shift);
+            }
+            else
+            {
+                throw new InvalidOperationException(String.Format("Character '{0}' at index '{1}' is not valid alphanumeric character.", character, index));
             }
 
             return value;
---
+++ src/BigMath/BigMath.Tests.NET45/StringUtilsFacts.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StringUtilsFacts.cs">
+//   Copyright (c) 2013 Alexander Logger. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BigMath.Utils;
+using NUnit.Framework;
+
+namespace BigMath.Tests
+{
+    [TestFixture]
+    public class StringUtilsFacts
+    {
+        [TestCaseSource("HexStringToByteArrayTestCases")]
+        public byte[] Should_convert_hex_string_to_array_of_bytes(string str)
+        {
+            return str.ConvertToByteArray();
+        }
+
+        private static IEnumerable HexStringToByteArrayTestCases
+        {
+            get { return GetHexStringToByteArrayTestCasesData.Select(hexBytese => new TestCaseData(hexBytese.Hex).Returns(hexBytese.Bytes)); }
+        }
+
+        private static IEnumerable<HexBytes> GetHexStringToByteArrayTestCasesData
+        {
+            get
+            {
+                yield return new HexBytes(string.Empty, new byte[0]);
+                yield return new HexBytes("0x0", new byte[] {0});
+                yield return new HexBytes("0x00", new byte[] {0});
+                yield return new HexBytes("0x0000", new byte[] {0, 0});
+                yield return new HexBytes("0x00000", new byte[] {0, 0, 0});
+                yield return new HexBytes("0x000000", new byte[] {0, 0, 0});
+                yield return new HexBytes("0x010203040506070809", Enumerable.Range(1, 9).Select(i => (byte) i).ToArray());
+                yield return new HexBytes("0x0102030405060708090A0B0C0D0E0F", Enumerable.Range(1, 15).Select(i => (byte) i).ToArray());
+                yield return new HexBytes("0x0102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F", Enumerable.Range(1, 31).Select(i => (byte) i).ToArray());
+            }
+        }
+
+        private class HexBytes
+        {
+            public HexBytes(string hex, byte[] bytes)
+            {
+                Hex = hex;
+                Bytes = bytes;
+            }
+
+            public string Hex { get; set; }
+            public byte[] Bytes { get; set; }
+        }
+    }
+}
