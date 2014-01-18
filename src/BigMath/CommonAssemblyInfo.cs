19d347f8f27e6d97736fe48634191ff99a871808
Alexander Logger <intagger@live.ru>
2014-01-19 01:39:48 +0400

    Updated version to 0.4.1.

--- src/BigMath/CommonAssemblyInfo.cs
+++ src/BigMath/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
