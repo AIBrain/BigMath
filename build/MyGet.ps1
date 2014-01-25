1e0aa8106f9867c36eff637bd97c80a39ff06e54
Alexander Logger <intagger@live.ru>
2014-01-25 23:35:28 +0400

    Updated MyGet build script.

--- build/MyGet.ps1
+++ build/MyGet.ps1
@@ -1,2 +1,14 @@
+param(
+    [string]$packageVersion = $null,
+    [string]$config = "Release",
+    [string[]]$targetFrameworks = @("v4.0", "v4.5", "v4.5.1"),
+    [string[]]$platforms = @("AnyCpu"),
+    [ValidateSet("rebuild", "build")]
+    [string]$target = "rebuild",
+    [ValidateSet("quiet", "minimal", "normal", "detailed", "diagnostic")]
+    [string]$verbosity = "minimal",
+    [bool]$alwaysClean = $true
+)
+
 $here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
-psake "$here/Default.ps1";
+psake "$here/Default.ps1" -properties "@{'config'=$config}";
